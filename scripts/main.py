c9a29011d84a580cd5e5a56a58a7d83663de03d9
Adnan Alouane <alouane@usc.edu>
2023-08-28 15:03:33 -0700

    can quiet songs now

--- scripts/main.py
+++ scripts/main.py
@@ -18,6 +18,7 @@ from kivy.uix.button import Button
 
 
 
+
 from pydub import AudioSegment
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -33,9 +34,80 @@ index = 0
 class MenuScreen(Screen):
     pass
 
-class EditScreen(Screen):
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty
+
+class TrimPopup(Popup):
+    start_time = StringProperty()
+    end_time = StringProperty()
+
+    def __init__(self, song_path, **kwargs):
+        super(TrimPopup, self).__init__(**kwargs)
+        self.song_path = song_path
+
     def trim_song(self):
-        pass
+        try:
+            start_time = int(self.start_time) * 1000  # Convert to milliseconds
+            end_time = int(self.end_time) * 1000  # Convert to milliseconds
+
+            parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            song_path = os.path.join(parent_directory, 'songs', self.song_path)  # Full path to the song
+
+            song = AudioSegment.from_mp3(song_path)  # Load the audio segment from the song file
+            trimmed_song = song[start_time:end_time]
+
+            # Prompt the user for a new filename
+            new_filename = input("Enter a new filename for the trimmed song (without extension): ")
+            if not new_filename:
+                print("Invalid filename. The song will be saved with a default name.")
+                new_filename = "trimmed_song"
+
+            trimmed_filename = f"{new_filename}.mp3"
+            trimmed_song_path = os.path.join(parent_directory, 'trimmedsongs', trimmed_filename)
+
+            trimmed_song.export(trimmed_song_path, format="mp3")
+            print(f"Song trimmed and saved as '{trimmed_filename}' in the 'trimmedsongs' folder")
+        except Exception as e:
+            print("Error trimming song:", e)
+
+class QuietPopup(Popup):
+    start_time = StringProperty()
+    end_time = StringProperty()
+
+    def __init__(self, song_path, **kwargs):
+        super(QuietPopup, self).__init__(**kwargs)
+        self.song_path = song_path
+
+    def quiet_song(self):
+        try:
+            start_time = int(self.start_time) * 1000  # Convert to milliseconds
+            end_time = int(self.end_time) * 1000  # Convert to milliseconds
+
+            parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            song_path = os.path.join(parent_directory, 'songs', self.song_path)  # Full path to the song
+
+            song = AudioSegment.from_mp3(song_path)  # Load the audio segment from the song file
+
+            # Apply the quiet effect to the specified time range
+            silence = AudioSegment.silent(duration=end_time - start_time)
+            quiet_part = song[:start_time] + silence + song[end_time:]
+
+            # Create a new filename for the modified song
+            new_filename = f"quiet_{os.path.basename(self.song_path)}"
+            quiet_song_path = os.path.join(parent_directory, 'quietsongs', new_filename)
+
+            # Export the modified song
+            quiet_part.export(quiet_song_path, format="mp3")
+            print(f"Song with quiet effect applied saved as '{new_filename}' in the 'quietsongs' folder")
+        except Exception as e:
+            print("Error applying quiet effect:", e)
+
+
+
 
 class TrimScreen(Screen):
     def __init__(self, **kwargs):
@@ -60,13 +132,36 @@ class TrimScreen(Screen):
     def select_song(self, button):
         selected_song = button.text
         print("Selected song:", selected_song)
-        # Perform further actions with the selected song
+        trim_popup = TrimPopup(title="Trim Song", song_path=selected_song)
+        trim_popup.open()
 
-
+class EditScreen(Screen):
+    def trim_song(self):
+        pass
 
 
 class QuietScreen(Screen):
-    pass
+    def on_pre_enter(self, *args):
+        self.populate_song_list()
+
+    def populate_song_list(self):
+        song_list_layout = BoxLayout(orientation='vertical')
+
+        for song_path in song_list:
+            song_button = Button(text=os.path.basename(song_path), on_release=self.open_quiet_popup)
+            song_list_layout.add_widget(song_button)
+
+        scroll_view = ScrollView()
+        scroll_view.add_widget(song_list_layout)
+        self.ids.song_list_container.add_widget(scroll_view)
+
+    def open_quiet_popup(self, button):
+        selected_song = button.text
+        print("Selected song:", selected_song)
+        quiet_popup = QuietPopup(title="Apply Quiet Effect", song_path=selected_song)
+        quiet_popup.open()
+
+
 class SpeedScreen(Screen):
     pass
 class SlowScreen(Screen):
